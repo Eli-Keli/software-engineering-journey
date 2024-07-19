cfe749efd4a28bd30e06b649c8095101e445e350
Eli Keli <muthokaeli@gmail.com>
2024-07-19 17:55:38 +0300

    Add new concepts on Day 2

--- Week1/Day2.py
+++ Week1/Day2.py
@@ -14,6 +14,29 @@ print(f"Name: {name}, Age: {age}, Height: {height}, Is Student: {is_student}")
 print(f"Sum of Age: {sum_age}, Height Squared: {height_squared},")
 
 
+# ----Variables----
+"""
+Creating Variables
+
+Python has no command for declaring a variable.
+A variable is created the moment you first assign a value to it.
+"""
+# Example
+x = 5
+y = "John"
+print(x)
+print(y)
+
+"""
+Casting
+If you want to specify the data type of a variable, this can be done with casting
+"""
+# Example
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+
 
 # Write a program that takes user input and performs basic arithmetic operations (addition, subtraction, multiplication, and division).
 
